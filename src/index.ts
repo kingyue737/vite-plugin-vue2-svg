903910bb8f4be04de602b2cd06815311e54aa2a1
Yue JIN <yjin@nustarnuclear.com>
2022-12-14 12:39:47 +0800

    feat: rewrite with compiler-sfc

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,67 @@
 import { readFileSync } from "fs";
-import { basename } from "path";
-import { optimize, Config } from "svgo";
+import { optimize, type Config } from "svgo";
+import { compileTemplate } from "@vue/compiler-sfc";
 import type { Plugin } from "vite";
-import { compileTemplate, parse } from "@vue/component-compiler-utils";
-import * as compiler from "vue-template-compiler";
-
-function compileSvg(svg: any, id: string): string {
-  const template = parse({
-    source: `
-      <template>
-        ${svg}
-      </template>
-    `,
-    compiler: compiler as any,
-    filename: `${basename(id)}.vue`,
-  }).template;
-
-  if (!template) return "";
-
-  const result = compileTemplate({
-    compiler: compiler as any,
-    source: template.content,
-    filename: `${basename(id)}.vue`,
-  });
-
-  return `
-    ${result.code}
-    export default {
-      render: render,
-    }
-  `;
-}
-
-function optimizeSvg(content: string, svgoConfig: Config) {
-  const result = optimize(content, svgoConfig);
-
-  if ("data" in result) {
-    return result.data;
-  }
-
-  throw new Error(
-    `[vite-plugin-vue2-svg] cannot optimize SVG ${svgoConfig.path}`,
-  );
-}
 
 export function createSvgPlugin(
   options: {
     svgoConfig?: Config;
+    svgo?: boolean;
+    defaultImport?: "url" | "raw";
   } = {},
 ): Plugin {
-  const { svgoConfig } = options;
-  const svgRegex = /\.svg$/;
+  const { svgoConfig, svgo, defaultImport } = options;
+  const svgRegex = /\.svg(\?(raw|component|skipsvgo))?$/;
 
   return {
     name: "vite-plugin-vue2-svg",
     enforce: "pre",
     async load(id: string) {
+      if (!id.match(svgRegex)) {
+        return;
+      }
+
+      const [path, query] = id.split("?", 2);
+      const importType = query || defaultImport;
       if (/\?raw/.test(id)) {
         return null;
       }
-
-      const fname = id.replace(/\?.*$/, "");
-      const isMatch = svgRegex.test(fname);
-      if (isMatch) {
-        const code: string = readFileSync(fname, { encoding: "utf-8" });
-        let svg = await optimizeSvg(code, { path: fname, ...svgoConfig });
-        if (!svg)
-          throw new Error(`[vite-plugin-vue2-svg] fail to compile ${id}`);
-        svg = svg.replace("<svg", '<svg v-on="$listeners"');
-        const result = compileSvg(svg, fname);
-
-        return result;
+      if (importType === "url") {
+        return; // Use default svg loader
       }
-      return null;
+
+      let svg: string;
+
+      try {
+        svg = readFileSync(path, { encoding: "utf-8" });
+      } catch (e) {
+        console.warn(
+          "\n",
+          `${id} couldn't be loaded by vite-plugin-vue2-svg, fallback to default loader`,
+        );
+        return;
+      }
+
+      if (importType === "raw") {
+        return `export default ${JSON.stringify(svg)}`;
+      }
+
+      if (svgo !== false && query !== "skipsvgo") {
+        svg = optimize(svg, {
+          ...svgoConfig,
+          path,
+        }).data;
+      }
+
+      svg = svg.replace("<svg", '<svg v-on="$listeners"');
+
+      const { code } = compileTemplate({
+        source: svg,
+        filename: path,
+        transformAssetUrls: false,
+      });
+
+      return `${code}\nexport default { render: render }`;
     },
   };
 }
